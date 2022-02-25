ea5d07541d013c427556b096530bcd051031623a
Edrikeddie <edwinwmachariake@gmail.com>
2022-02-25 17:44:30 +0300

    Movie Tracking

---
+++ MovieTicketing/Data/MovieCategory.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MovieTicketing.Data
+{
+    public enum MovieCategory
+    {
+        Action = 1,
+        Drama,
+        Documentaries,
+        War,
+
+    }
+}
