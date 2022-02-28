2353a5145fdbdf028ccacb14f572b0edb730eb76
Edrikeddie <edwinwmachariake@gmail.com>
2022-02-28 06:37:06 +0300

    First Commit

--- MovieTicketing/Data/AppDbContext.cs
+++ MovieTicketing/Data/AppDbContext.cs
@@ -18,9 +18,17 @@ namespace MovieTicketing.Data
             modelBuilder.Entity<Actor_Movie>().HasKey(am => new
             {
                 am.ActorId,
-                am.MovieId,
+                am.MovieId
             });
+            modelBuilder.Entity<Actor_Movie>().HasOne(m => m.Movie).WithMany(am => am.Actors_Movies).HasForeignKey(m => m.MovieId);
+            modelBuilder.Entity<Actor_Movie>().HasOne(m => m.Actor).WithMany(am => am.Actors_Movies).HasForeignKey(m => m.ActorId);
             base.OnModelCreating(modelBuilder);
         }
+        public DbSet<Actor> Actors { get; set; }
+        public DbSet<Movie> Movies { get; set; }
+        public DbSet<Actor_Movie> Actors_Movies { get; set; }
+        public DbSet<Cinema> Cinemas { get; set; }
+        public DbSet<Producer> Producers { get; set; }
+
     }
 }
