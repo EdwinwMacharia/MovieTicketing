39761d0bdb29eeed5c358c799cc1c0b3568b7eb2
Edwin <edwinwmacharia@gmail.com>
2022-03-01 14:40:53 +0300

    Second commit

---
+++ MovieTicketing/Data/AppDbInitializer.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace MovieTicketing.Data
+{
+    public class AppDbInitializer
+    {
+        public static void Seed(IApplicationBuilder applicationBuilder)
+        {
+            using(var serviceScope = applicationBuilder.ApplicationServices.CreateScope())
+            {
+
+            }
+
+        }
+    }
+}
